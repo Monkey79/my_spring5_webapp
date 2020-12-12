2d2ccfed675e0c42cfbbfd86b52c935eecf7eb16
Monkey79 <marianomartin79@gmail.com>
2020-12-12 10:29:27 -0300

    spring boot app enviroment

---
+++ src/main/java/com/mdangelo/spring5_webapp/Spring5WebappApplication.java
@@ -0,0 +1,13 @@
+package com.mdangelo.spring5_webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5WebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5WebappApplication.class, args);
+	}
+
+}
